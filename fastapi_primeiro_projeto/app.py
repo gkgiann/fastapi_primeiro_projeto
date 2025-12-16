b62034f07edabeda503b53e5d7cced313e8ca97f
gian <lgian584@gmail.com>
2025-12-16 14:01:13 -0300

    feat: rotas de crud (db em memória) e testes

--- fastapi_primeiro_projeto/app.py
+++ fastapi_primeiro_projeto/app.py
@@ -1,12 +1,68 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from fastapi_primeiro_projeto.schemas import Message
+from fastapi_primeiro_projeto.schemas import (
+    UserDB,
+    UserList,
+    UserPublic,
+    UserSchema,
+)
 
 app = FastAPI()
 
+database = []
 
-@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
-def read_root():
-    return {'message': 'Hello World'}
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def get_users():
+    return {'users': database}
+
+
+@app.get(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def get_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            detail='User not found!', status_code=HTTPStatus.NOT_FOUND
+        )
+
+    user = database[user_id - 1]
+
+    return user
+
+
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            detail='User not found!', status_code=HTTPStatus.NOT_FOUND
+        )
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', status_code=HTTPStatus.NO_CONTENT)
+def remove_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            detail='User not found!', status_code=HTTPStatus.NOT_FOUND
+        )
+
+    database.pop(user_id - 1)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,84 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fastapi_primeiro_projeto.app import app
+def test_create_user(client):
+    request = client.post(
+        '/users/',
+        json={'username': 'gian', 'email': 'gian@email.com', 'password': '13'},
+    )
+
+    assert request.status_code == HTTPStatus.CREATED
+    assert request.json() == {
+        'id': 1,
+        'username': 'gian',
+        'email': 'gian@email.com',
+    }
 
 
-def test_root_deve_retornar_hello_world():
-    client = TestClient(app)
-    res = client.get('/')
-    assert res.json() == {'message': 'Hello World'}
-    assert res.status_code == HTTPStatus.OK
+def test_get_users(client):
+    request = client.get('/users/')
+
+    assert request.status_code == HTTPStatus.OK
+    assert request.json() == {
+        'users': [{'username': 'gian', 'email': 'gian@email.com', 'id': 1}]
+    }
+
+
+def test_get_user(client):
+    request = client.get('/users/1')
+
+    assert request.status_code == HTTPStatus.OK
+    assert request.json() == {
+        'username': 'gian',
+        'email': 'gian@email.com',
+        'id': 1,
+    }
+
+
+def test_get_user_not_found(client):
+    request = client.get('/users/567')
+    assert request.status_code == HTTPStatus.NOT_FOUND
+    assert request.json() == {'detail': 'User not found!'}
+
+
+def test_update_user(client):
+    request = client.put(
+        '/users/1',
+        json={
+            'username': 'geraldo',
+            'email': 'geraldo@email.com',
+            'password': 'snehaboa',
+        },
+    )
+
+    assert request.status_code == HTTPStatus.OK
+    assert request.json() == {
+        'username': 'geraldo',
+        'email': 'geraldo@email.com',
+        'id': 1,
+    }
+
+
+def test_update_user_not_found(client):
+    request = client.put(
+        '/users/7',
+        json={
+            'username': 'geraldo',
+            'email': 'geraldo@email.com',
+            'password': 'snehaboa',
+        },
+    )
+
+    assert request.status_code == HTTPStatus.NOT_FOUND
+    assert request.json() == {'detail': 'User not found!'}
+
+
+def test_remove_user(client):
+    request_delete = client.delete('/users/1/')
+    assert request_delete.status_code == HTTPStatus.NO_CONTENT
+
+
+def test_remove_user_not_found(client):
+    request = client.delete('/users/73')
+    assert request.status_code == HTTPStatus.NOT_FOUND
+    assert request.json() == {'detail': 'User not found!'}
